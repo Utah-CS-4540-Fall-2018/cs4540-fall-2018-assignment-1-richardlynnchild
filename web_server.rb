02f7c4fa2d8dd726e7a57e20b9a1ef70e1cd814a
richardlynnchild <richardlynnchild@gmail.com>
2018-08-30 11:29:25 -0600

    Created the create_response() function

--- web_server.rb
+++ web_server.rb
@@ -21,7 +21,8 @@ class WebServer
 
     end
 
-    # Parses the incoming request from the client
+    # Collects the incoming request from the client and puts each header
+    # and line into an array.
     def collect_request()
         @socket = @server.accept
         @request_lines = []
@@ -31,6 +32,23 @@ class WebServer
             line = @socket.gets.strip
         end
         
+    # Parses the incoming request information and pulls out the User-Agent
+    # information. The creates a HTTP OK response with the 'Hello' message.
+    def create_response()
+        @request_lines.each do |line|
+            words = line.split
+            if words[0] == "User-Agent:"
+                agent = words[1]
+            end
+            
+        end
+        greeting = "Hello #{agent} user!\n"
+        @response =  "HTTP/1.1 200 OK\r\n" +
+                    "Content-Type: text/plain\r\n" +
+                    "Content-Length: #{greeting.bytesize}\r\n" +
+                    "Connection: close\r\n\r\n" + greeting
+
+    end
 
 
 
